59e6a05b9a6268d99c6ba09f97f16c63e21bdbc8
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2022-02-20 23:19:43 -0800

    Rename module and update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name="simple-mp",
+    name="simple-python",
     version="1.0",
     description="An implementation of the SiMPle algorithm",
     author="Anthony Miyaguchi",
     author_email="acmiyaguchi@gmail.com",
-    url="https://github.com/acmiyaguchi/birdclef-2021",
-    packages=["simple_mp"],
+    url="https://github.com/acmiyaguchi/simple-python",
+    packages=["simple"],
     install_requires=[
         "numpy",
     ],
