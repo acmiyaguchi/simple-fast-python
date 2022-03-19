02a1a025085907c53c0ae7a38f7f92189ca88252
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2022-03-19 01:03:09 -0700

    Change version to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="simple-fast-python",
-    version="1.0",
+    version="1.0.0",
     description="An implementation of the SiMPle-Fast algorithm",
     author="Anthony Miyaguchi",
     author_email="acmiyaguchi@gmail.com",
